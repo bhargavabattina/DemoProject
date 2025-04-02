8c7f6cc38cea54f8d8a57cffa39a963c27f241e9
bhargavabattina <bhargavbattina24@gmail.com>
2025-04-02 11:44:41 +0530

    first

--- utilites/readProperties.py
+++ utilites/readProperties.py
@@ -1,20 +1,22 @@
-import os
 import configparser
 
-# Get the correct path for both local and CI
-config_path = os.path.join(os.path.dirname(__file__), "Configuration", "config.ini")
-config = configparser.RawConfigParser()
-config.read(config_path)
+
+config=configparser.RawConfigParser()
+config.read("./Configuration/config.ini")
 
 class ReadConfig:
     @staticmethod
     def getApplicationUrl():
-        return os.getenv("BASE_URL") or config.get('info', 'baseURL')
+        url=config.get('info','baseURL')
+        return url
 
     @staticmethod
     def getUserName():
-        return os.getenv("USERNAME") or config.get('login', 'username')
+        username = config.get('login', 'username')
+        return username
 
     @staticmethod
     def getPassword():
-        return os.getenv("PASSWORD") or config.get('login', 'password')
+        password = config.get('login', 'password')
+        return password
+
