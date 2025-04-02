16150a3529d9e4f43f482c2296e2812a5ad90072
bhargavabattina <bhargavbattina24@gmail.com>
2025-04-02 15:57:58 +0530

    first

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from testCases.conftest import TEAMS_WEBHOOK_URL
+
 
 URL_DATA = {
     "orange_url": "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
@@ -14,6 +14,6 @@ LOGIN_DATA = {
 }
 
 
-TEAMS_WEBHOOK_URL ={
+TEAMS_WEBHOOK_URL = {
     "teams_webhook_url": "https://srslivetech.webhook.office.com/webhookb2/e16c66cb-8396-4759-befc-8d4462ee1d31@4f1f5a9f-f6a8-4bc9-a157-7fddfe22b5f6/IncomingWebhook/60bfc1d3c89d4f72bdc44e674ee9b38d/3e17875b-b12b-400d-adc2-eefc03796a3a/V27HSL3GH65e9-gmNmRADasY76FK4I1nYMKvpJylj74RE1",
 }
